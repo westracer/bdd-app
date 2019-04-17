643d569f9a9371823d56fefeff6abf18c2a77d4e
Igor Kharakhordin <westracer1@gmail.com>
2019-04-17 20:25:01 +0700

    + Функции генерации аккордов и toString

--- src/test/java/model/Chord.java
+++ src/test/java/model/Chord.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+
 public class Chord {
     public ChordRoot root;
     public ChordType type;
@@ -7,6 +9,14 @@ public class Chord {
     public Chord(ChordRoot root, ChordType type) {
         this.root = root;
         this.type = type;
+    }
+
+    public static Chord generate() {
+        ChordRoot[] roots = ChordRoot.values();
+        ChordType[] types = ChordType.values();
+        Random r = new Random();
+
+        return new Chord(roots[r.nextInt(roots.length)], types[r.nextInt(types.length)]);
     }
 
     @Override
@@ -18,5 +28,40 @@ public class Chord {
         } else {
             return super.equals(obj);
         }
+    }
+
+    @Override
+    public String toString() {
+        String rootName, typeName;
+        switch (root) {
+            case As:
+            case Cs:
+            case Ds:
+            case Gs:
+            case Fs:
+                rootName = root.toString().toCharArray()[0] + "#";
+                break;
+            default:
+                rootName = root.toString();
+        }
+
+        switch (type) {
+            case five:
+                typeName = "5";
+                break;
+            case seven:
+                typeName = "7";
+                break;
+            case major:
+                typeName = "";
+                break;
+            case minor:
+                typeName = "m";
+                break;
+            default:
+                typeName = "";
+        }
+
+        return rootName + typeName;
     }
 }
